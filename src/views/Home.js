70a2b3e32d2030a9c894789b93d5496f72c99381
luvswallow <980925086@qq.com>
2024-07-23 16:04:24 +0800

    /dashboard下的表的初始形态

---
+++ src/views/Home.js
@@ -0,0 +1,37 @@
+import { resetPassword } from '@/api/login'
+
+export default {
+    name:'HomeView',
+    data(){
+        return{
+                tableData : [
+                    {
+                        date: '2016-05-03',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles',
+                    },
+                    {
+                        date: '2016-05-02',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles',
+                    },
+                    {
+                        date: '2016-05-04',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles',
+                    },
+                    {
+                        date: '2016-05-01',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles',
+                    },
+                ]
+            }
+        },
+
+    methods:{
+        handleDelete(){
+
+        },
+    }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="common-layout">
+    <el-container>
+      <el-header>表</el-header>
+      <el-container>
+        <el-aside width="200px">左</el-aside>
+        <el-main>
+          <el-table :data="tableData" style="width: 100%" stripe height="250">
+            <el-table-column prop="date" label="1" width="180" />
+            <el-table-column prop="name" label="2" width="180" />
+            <el-table-column prop="address" label="3" />
+            <el-table-column fixed="right" label="4" width="120">
+              <template #default>
+                <el-button link type="primary" size="small" @click="handleDelete">
+                  删除
+                </el-button>
+                <el-button link type="primary" size="small">
+                  Edit
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from './Home.js'
+export default Home
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
